81ad231bf30bb113132cb345e13923542d5b67b9
awewsomegamer <awewsomegamer@gmail.com>
2025-07-05 04:31:33 -0500

    info: acquire information about the architecture
    This new header will allow for more information to be gathered about the
    architecture. Currently I just use it to acquire the maximum address
    widths (physical + virtual).

---
+++ src/c/include/arch/info.h
@@ -0,0 +1,35 @@
+/**
+ * @file info.h
+ *
+ * @author awewsomegamer <awewsomegamer@gmail.com>
+ *
+ * @LICENSE
+ * Arctan-OS/Kernel - Operating System Kernel
+ * Copyright (C) 2023-2025 awewsomegamer
+ *
+ * This file is part of Arctan-OS/Kernel.
+ *
+ * Arctan is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; version 2
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * @DESCRIPTION
+*/
+#ifndef ARC_ARCH_INFO_H
+#define ARC_ARCH_INFO_H
+
+#include <stdint.h>
+
+uint32_t arch_physical_address_width();
+uint32_t arch_virtual_address_width();
+
+#endif
