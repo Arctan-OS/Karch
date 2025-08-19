3d5872ed7395100a556ce75000b8ad0912dee447
awewsomegamer <awewsomegamer@gmail.com>
2025-08-19 16:54:03 -0500

    smp: further abstractions
    The previous header file I found to be too dependent on x86-64, so I
    have made this a bit more abstract. The more specific things can be
    defined in the architecture specific implementation.

--- src/c/include/arch/smp.h
+++ src/c/include/arch/smp.h
@@ -29,35 +29,22 @@
 #ifndef ARC_ARCH_SMP_H
 #define ARC_ARCH_SMP_H
 
+#include <stdint.h>
+
+#include "arch/context.h"
+#include "mp/scheduler.h"
+#include "userspace/thread.h"
+
 // Offsets into flags attribute
-#define ARC_SMP_FLAGS_RESV0 0
-#define ARC_SMP_FLAGS_RESV1 1
-#define ARC_SMP_FLAGS_WTIMER 2
-#define ARC_SMP_FLAGS_HOLD 30
-#define ARC_SMP_FLAGS_INIT 31
+enum {
+        ARC_SMP_FLAGS_RESV0  = 0,
+        ARC_SMP_FLAGS_RESV1  = 1,
+        ARC_SMP_FLAGS_WTIMER = 2,
+        ARC_SMP_FLAGS_HOLD   = 30,
+        ARC_SMP_FLAGS_INIT   = 31,
+};
 
-#include <stdint.h>
-#include <lib/atomics.h>
-#include <config.h>
-
-#include <stdint.h>
-#include <arch/context.h>
-#include <lib/atomics.h>
-#include <stdarg.h>
-#include <userspace/thread.h>
-#include <userspace/process.h>
-
-#ifdef ARC_TARGET_ARCH_X86_64
-#include <arch/x86-64/apic/lapic.h>
-#endif
-
-// TODO: This structure can be changed such that there is no need for syscall_stack
-//       by making it so that the KernelGS pointer points to the stack, and on the stack
-//       is this structure. This would also eliminate the need for a processor descriptor list.
-struct ARC_ProcessorDescriptor {
-	uintptr_t syscall_stack;
-	struct ARC_ProcessorDescriptor *next;
-	
+typedef struct ARC_ProcessorDescriptor {
 	struct ARC_Thread *current_thread;
 	struct ARC_ProcessEntry *current_process;
 	void *scheduler_meta;
@@ -74,16 +61,8 @@ struct ARC_ProcessorDescriptor {
 
 	uint32_t timer_ticks;
 	uint32_t timer_mode;
-}__attribute__((packed));
+} ARC_ProcessorDescriptor;
 
-// NOTE: The index in Arc_ProcessorList corresponds to the ID
-//       acquired from get_processor_id();
-extern struct ARC_ProcessorDescriptor Arc_ProcessorList[ARC_MAX_PROCESSORS];
-// NOTE: Since the processor structure includes a next pointer,
-//       this allows us to also traverse the list by processor,
-//       while still being able to address the list by processor ID
-//       in O(1) time
-extern struct ARC_ProcessorDescriptor *Arc_BootProcessor;
 extern uint32_t Arc_ProcessorCounter;
 
 /**
@@ -91,29 +70,12 @@ extern uint32_t Arc_ProcessorCounter;
  * */
 void smp_hold();
 
-int smp_switch_to_userspace();
-
-struct ARC_ProcessorDescriptor *smp_get_proc_desc();
+ARC_ProcessorDescriptor *smp_get_proc_desc();
 
 uint32_t smp_get_processor_id();
 
-int smp_set_tcb(void *tcb);
+void smp_switch_to(ARC_Context *ctx);
 
-/**
- * Initialize an AP into an SMP system.
- *
- * Initializes the given AP into an SMP system by creating the
- * relevant processor descriptors and sending INIT and START IPIs.
- *
- * NOTE: This functions is meant to only be called from the BSP.
- * NOTE: This function should be called to initialize the BSP as well
- *       since it needs to be inserted into the descriptor list.
- * @param uint32_t lapic - The ID of the LAPIC to initialize.
- * @param uint32_t acpi_uid - The UID given to the processor by ACPI.
- * @param uint32_t acpi_flags - The flags given by ACPI.
- * @param uint32_t version - The version of the LAPIC.
- * @return zero upon success.
- * */
-int init_smp(uint32_t lapic, uint32_t acpi_uid, uint32_t acpi_flags, uint32_t version);
+int init_smp();
 
 #endif
